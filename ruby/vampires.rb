d50ea53102d0c2730a7cbbe0ff13ccc73ac5491f
miker-mcd <michaelrmcd@gmail.com>
2017-05-30 14:28:00 -0700

    Write new employee questions for vampire-detection

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+
+employee_name = gets.chomp
+
+puts "How old are you?"
+
+employee_age = gets.chomp
+
+puts "What year were you born?"
+
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+insurance = gets.chomp
