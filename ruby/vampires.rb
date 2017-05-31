72c9dd2efae0df3884823d62e728ad6488f8cc2d
miker-mcd <michaelrmcd@gmail.com>
2017-05-31 14:06:46 -0700

    Add survey feedback to test variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,35 +2,64 @@ puts "What is your name?"
 
 employee_name = gets.chomp
 
+puts "Your name is #{employee_name}."
+
 puts "How old are you?"
 
 employee_age = gets.chomp
+
+employee_age = employee_age.to_i
+
+puts "You are #{employee_age} years old."
 
 puts "What year were you born?"
 
 employee_year = gets.chomp
 
+employee_year = employee_year.to_i
+
+puts "You were born in #{employee_year}."
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 
 garlic_bread = gets.chomp
+
+if garlic_bread == "y"
+  garlic_bread = true
+  puts "You would like garlic bread."
+elsif garlic_bread == "n"
+  garlic_bread = false
+  puts "You do not want garlic bread"
+end
 
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 
 insurance = gets.chomp
 
-wolves_like_sunshine = true
+if insurance == "y"
+  insurance = true
+  puts "You would like to enroll."
+elsif insurance == "n"
+  insurance = false
+  puts "You do not want to enroll."
+end
+
+=beginwolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
+=end
 
 time = Time.new
 
-correct_age = time.year - employee_year.to_i
-correct_age2 = time.year - employee_year.to_i - 1
+correct_age = time.year - employee_year
+# correct_age2 = time.year - employee_year
 
+=begin
 case
   when employee_age == correct_age || correct_age2 && garlic_bread == "y" || insurance == "y"
     puts "Probably not a vampire."
   else
     puts "Results inconclusive"
   end
+=end
