2a5ee0488d941306cdaa74ae79c76ee5b51d8e80
miker-mcd <michaelrmcd@gmail.com>
2017-05-31 10:48:46 -0700

    Test case statement for first condition

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,12 +8,29 @@ employee_age = gets.chomp
 
 puts "What year were you born?"
 
-birth_year = gets.chomp
+employee_year = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 
 garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 
 insurance = gets.chomp
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+time = Time.new
+
+correct_age = time.year - employee_year.to_i
+correct_age2 = time.year - employee_year.to_i - 1
+
+case
+  when employee_age == correct_age || correct_age2 && garlic_bread == "y" || insurance == "y"
+    puts "Probably not a vampire."
+  else
+    puts "Results inconclusive"
+  end
