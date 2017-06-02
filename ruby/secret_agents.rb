8781033a206100123a25d7f895eb276acddca5f7
miker-mcd <michaelrmcd@gmail.com>
2017-06-02 15:34:31 -0700

    Add encrypt method from pair

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,8 +1,24 @@
 =begin
 
 encrypt method
-- while string does not equal a space
-  - if string is a space
+- while index is less than the length of the string
+  - if string contains a space
     - leave unchanged and move to next letter
+  - Otherwise:
   - Advance each letter of the string one letter forward. Ex. input = "abc" output = "bcd"
 
+alpha = "abc"
+index = 0
+
+while index < alpha.length
+  alpha[index] = alpha[index].delete(" ")
+  puts alpha[index] = alpha[index].next
+  index += 1
+  end
+
+encrypt method
+- while index is less than the length of the string
+  - if string contains a space
+    - leave unchanged and move to next letter
+  - Otherwise:
+  - Advance each letter of the string one letter forward. Ex. input = "abc" output = "bcd"
