69a4622cbd8ab08656cee8f749cb74d6b54af170
miker-mcd <michaelrmcd@gmail.com>
2017-06-02 09:00:26 -0700

    Pseudocode test encrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,8 @@
+=begin
+
+encrypt method
+- while string does not equal a space
+  - if string is a space
+    - leave unchanged and move to next letter
+  - Advance each letter of the string one letter forward. Ex. input = "abc" output = "bcd"
+
