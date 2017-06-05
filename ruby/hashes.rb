7d20ea69c19f100ecf328743bbd19447504b01a4
miker-mcd <michaelrmcd@gmail.com>
2017-06-05 13:28:36 -0700

    Add application_5.2 file and pseudocode user preference program

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+=begin
+  Interior Designer Preference
+  - Prompt designer/user for preference information.
+  - Convert user input to appropriate data type.
+  - Display the preference information to the user after all questions are answered.
+    - if designer/user made a mistake.
+      - allow for one edit to a preference.
+    - if user did not make a mistake.
+      - prompt user to type "none".
+  - Display latest version of preference information and exit program.
+  =end
+
