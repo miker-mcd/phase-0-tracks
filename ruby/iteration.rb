d45943eb33ec0be5cba3e7b81a8bfc26178db358
miker-mcd <michaelrmcd@gmail.com>
2017-06-06 11:56:53 -0700

    Test sample block

---
+++ ruby/iteration.rb
@@ -0,0 +1,7 @@
+def print_method
+  puts "This is before the block."
+  yield("Gloria", "Michael")
+  puts "This is after the block."
+end
+
+print_method { |name1, name2| puts "#{name1 } and #{name2}." }
