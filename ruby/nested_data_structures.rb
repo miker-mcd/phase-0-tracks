44f3dc2639319808ff4530a01730a17145c96620
miker-mcd <michaelrmcd@gmail.com>
2017-06-06 17:56:59 -0700

    Add apple data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+=begin
+
+phone_companies = {
+  Apple: {
+    iphone7: {
+      release date: "Sep 16, 2016",
+      display: "4.7 in",
+      generation: 10
+      },
+    c_level: ["Tim Cook", "Jon Ive", "Luca Maestri"]
+    years_old: 41
+    }
+  }
+}
+end
