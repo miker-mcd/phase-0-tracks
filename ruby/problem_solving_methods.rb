159dc3a33a0a566c051b5bae586a271460e43dc7
miker-mcd <michaelrmcd@gmail.com>
2017-06-09 16:28:05 -0700

    Test method for Release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,33 @@
 arr = [42, 89, 23, 1]
-p search_array(arr, 1)
-p search_array(arr, 24)
+# index = 0
+# p search_array(arr, 1)
+# p search_array(arr, 24)
+# don't use built-in array methods like .index
+
+def search_array(arr, x)
+
+  arr.each do |number|
+    if x == number
+
+      # p arr.index(number)
+end
+end
+end
+
+# search_array(arr, 89)
+
+def search_array(arr, x)
+count = 0
+while count < arr.length
+  if x != arr[count]
+    p 'nil'
+    break
+  elsif x == arr[count]
+    p count
+  end
+count += 1
+end
+end
+
+search_array(arr, 1)
+search_array(arr, 24)
