27dee5b1f0d3a10f40207753bf4f7d8590aafbf0
miker-mcd <michaelrmcd@gmail.com>
2017-06-10 13:21:58 -0700

    Test returns for fib method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,5 @@
 ### Release 0 Simple Search
 
-# index = 0
 # p search_array(arr, 1)
 # p search_array(arr, 24)
 # don't use built-in array methods like .index
@@ -22,10 +21,27 @@ count += 1
 end
 end
 
-p search_array(arr, 1)
-p search_array(arr, 24)
+# p search_array(arr, 1)
+# p search_array(arr, 24)
 
 # Why do both declarations return: 3, nil, nil, ' '? - p returns nil, but second p return ' '.
 
 ### Release 1 Calculate Fibonacci Numbers
 
+# Start with an array of 0 and 1 and add new numbers into the array by adding the previous two. Each number in the array is the sum of the previous two numbers, except for 0 and 1.
+
+# Method that TAKES a NUMBER of Fibonacci terms to generate and RETURNS an array of the terms.
+# fib(6) => [0,1,1,2,3,5]
+# fib(100) => [0,1,..218922995834555169026]
+# x = number of times to add a new number.
+
+def fib
+fibo_arr = [2, 3]
+  fibo_arr.each do |number|
+
+  number + fibo_arr[fibo_arr.index(number) - 1]
+
+  end
+end
+
+p fib
