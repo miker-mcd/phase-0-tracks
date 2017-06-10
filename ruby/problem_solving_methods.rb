82cf409a6df5f2a013bb7b068c12286b4924f195
miker-mcd <michaelrmcd@gmail.com>
2017-06-10 09:47:08 -0700

    Test and complete search_array method for release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,33 +1,31 @@
-arr = [42, 89, 23, 1]
+### Release 0 Simple Search
+
 # index = 0
 # p search_array(arr, 1)
 # p search_array(arr, 24)
 # don't use built-in array methods like .index
 
-def search_array(arr, x)
+arr = [42, 89, 23, 1]
 
-  arr.each do |number|
-    if x == number
-
-      # p arr.index(number)
-end
-end
-end
-
-# search_array(arr, 89)
+# method that takes array of integers and an integer to search for.
+# output: index of item or nil if item is not present in the array.
 
 def search_array(arr, x)
 count = 0
 while count < arr.length
-  if x != arr[count]
-    p 'nil'
-    break
-  elsif x == arr[count]
+  if x == arr[count]
     p count
+  elsif arr[0..-1] == x
+    arr[x]
   end
 count += 1
 end
 end
 
-search_array(arr, 1)
-search_array(arr, 24)
+p search_array(arr, 1)
+p search_array(arr, 24)
+
+# Why do both declarations return: 3, nil, nil, ' '? - p returns nil, but second p return ' '.
+
+### Release 1 Calculate Fibonacci Numbers
+
