d57ae1d1ab2ecd6576764856669720e1d7a8e2e2
miker-mcd <michaelrmcd@gmail.com>
2017-06-11 15:47:47 -0700

    Refactor and update indents for readability

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,78 +10,73 @@
   - Display latest version of preference information and exit program.
 =end
 
+# DRIVER CODE
+
 # Set up empty hash to put preference information into.
-preference = {
-=begin
-  full_name: string
-  age: integer
-  children: integer
-  decor_theme: ["french", "modern", "socal"]
-  college_grad: boolean
-  hgtv: boolean
-  bedrooms: integer
-=end
-}
+preference = {}
 
 # Collect client preference information
 puts "Client's full name:"
 preference[:full_name] = gets.chomp
 
 puts "Client age:"
-preference[:age] = gets.chomp
-preference[:age] = preference[:age].to_i
+preference[:age] = gets.chomp.to_i
 
 puts "Client number of children:"
-preference[:children] = gets.chomp
-preference[:children] = preference[:children].to_i
+preference[:children] = gets.chomp.to_i
 
 puts "Preferred decor theme:"
 preference[:decor_theme] = gets.chomp
 
 puts "College grad(y/n):"
-preference[:college_grad] = gets.chomp
-if preference[:college_grad] == "y"
-  preference[:college_grad] = true
-elsif preference[:college_grad] == "n"
-  preference[:college_grad] = false
-end
+college = gets.chomp
+
+  if college == "y"
+    preference[:college] = true
+  elsif college == "n"
+    preference[:college] = false
+  end
 
 puts "Does the client watch HGTV(y/n)?"
-preference[:hgtv] = gets.chomp
-if preference[:hgtv] == "y"
-  preference[:hgtv] = true
-elsif preference[:hgtv] == "n"
-  preference[:hgtv] = false
-end
+hgtv = gets.chomp
+
+  if hgtv == "y"
+    preference[:hgtv] = true
+  elsif hgtv == "n"
+    preference[:hgtv] = false
+  end
 
 puts "Number of bedrooms:"
-preference[:bedrooms] = gets.chomp
-preference[:bedrooms] = preference[:bedrooms].to_i
+preference[:bedrooms] = gets.chomp.to_i
 
 puts "Here is the client information: #{preference}"
 
 # Prompt user to fix preference key mistake.
 puts "If you made a mistake, please enter the name of the preference category to fix, otherwise type 'none' to finish."
 mistake = gets.chomp
+preference_fix = mistake.to_sym
 
 # If response is "none" then skip.
-if mistake == "none"
-  puts "Client preferences completed, thank you."
+  if mistake == "none"
+    puts "Client preferences completed, thank you."
+
 # Prompt user to enter correct preference value.
-else
-  puts "Please enter new value for preference"
-  preference_fix = mistake.to_sym
-  data_fix = gets.chomp
-  preference[preference_fix] = data_fix
-  if data_fix == "y"
-    preference[preference_fix] = true
-  elsif data_fix == "n"
-    preference[preference_fix] = false
-  elsif preference_fix == :age || :children || :bedrooms
-      preference[preference_fix] = data_fix.to_i
-  # Display updated version of preferences to user.
-  puts "Here is the latest version of the client preferences: #{preference}"
-  # End greeting.
-  puts "Thank you!"
-end
-end
+  else
+    puts "Please enter new value for preference"
+    data_fix = gets.chomp
+    preference[preference_fix] = data_fix
+
+    if data_fix == "y"
+      preference[preference_fix] = true
+    elsif data_fix == "n"
+      preference[preference_fix] = false
+    elsif preference_fix == :age || :children || :bedrooms
+        preference[preference_fix] = data_fix.to_i
+    end
+  end
+
+# Display updated version of preferences to user.
+puts "Here is the latest version of the client preferences: #{preference}"
+
+# End greeting.
+puts "Thank you!"
