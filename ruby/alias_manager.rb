31b007e94ed6843001966b654db84daf702d7470
miker-mcd <michaelrmcd@gmail.com>
2017-06-11 16:47:59 -0700

    Refactor, clean and add driver code for readability

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,93 +2,84 @@
 Alias Name Algorithm
 
 - While user provides a name when prompted:
-  # gets.chomp - string
   - Swap the first and last name.
-  # .split empty - array, .reverse?
   - While iterating through each letter
-   # .split(''), .map! block method with  conditionals
-    - If there is a vowel in the name (a, e,  i, o, u), change to the next vowel in  'aeiou'
-   # letter.include?("aeiou") --> either  match to and (+1) advance within a e i o u array or build vowel conditionals in new method and pass in letter as argument to return new vowel ex. if letter == "a" then  puts "e" ...
+    - If there is a vowel in the name (a, e, i, o, u), change to the next vowel in  'aeiou'
    - If there is a consonant in the name,  change the letter to the next consonant in  the alphabet. Ex. 'a' --> 'e' and 'd' -->  'f'.
-   # letter.include?('bcdfghjklmnpqrstvwxyz')  --> either match to and (+1) advance  within consonant array or build consonant  conditionals in new method and pass in  letter as argument to return new consonant  ex. if letter == "d" then puts "f" ...
-   .next only for ASCII? does it take  arguments?
   - If user types 'quit', then exit the program.
 Otherwise:
   - If user hits enter or input is nil, then continue program.
 =end
 
-def alias_method(letters_array)
-
-letters_array.map! do |letter|
-
-case
-when letter == 'a'
-  letter = 'e'
-when letter == 'e'
-  letter = 'i'
-when letter == 'i'
-  letter = 'o'
-when letter == 'o'
-  letter = 'u'
-when letter == 'u'
-  letter = 'a'
-when letter == 'd'
-  letter = 'f'
-when letter == 'h'
-  letter = 'j'
-when letter == 'n'
-  letter = 'p'
-when letter == 't'
-  letter = 'v'
-when letter == 'z'
-  letter = 'b'
-when letter == 'Z'
-  letter = 'A'
-else
-  letter = letter.next
+# Create a method that changes a vowel to the next vowel and a consonant to the next consonant and returns the letters joined together as one string.
+def alias_method(array_of_letters)
+  array_of_letters.map! do |letter|
+    case
+      when letter == 'a'
+        letter = 'e'
+      when letter == 'e'
+        letter = 'i'
+      when letter == 'i'
+        letter = 'o'
+      when letter == 'o'
+        letter = 'u'
+      when letter == 'u'
+        letter = 'a'
+      when letter == 'd'
+        letter = 'f'
+      when letter == 'h'
+        letter = 'j'
+      when letter == 'n'
+        letter = 'p'
+      when letter == 't'
+        letter = 'v'
+      when letter == 'z'
+        letter = 'b'
+      when letter == 'Z'
+        letter = 'A'
+      else
+        letter = letter.next
+      end
+    end
+  array_of_letters.join('')
 end
 
-end
-letters_array.join('')
-end
-
-######## User interface
+# Set up an empty string for a name.
+# Set up an empty hash to store the real name and alias name pairs.
 
 real_name = ''
 alias_list = {}
 
+# Set up a loop to receive a name as input until the user types 'quit' to end the program.
 until real_name == 'quit'
 
+# Prompt user for a name.
 puts 'Please enter full name or type \'quit\' to continue.'
 real_name = gets.chomp
 
-if real_name == 'quit'
-  puts "Exit program"
-elsif real_name == ''
-  puts "A name was not provided."
-  break
-else
+# If input is 'quit' or '' exit program.
+  if real_name == 'quit'
+   p "Exit program"
+  elsif real_name == ''
+   p "A name was not provided."
+   break
+  else
 
-reverse_name = real_name.split.reverse!
-# ['Last', 'First'] <-- array
+# Create array of letters of name after reversing.
+reverse_array = real_name.split.reverse!
+reverse_name = reverse_array.join(' ')
+reverse_letters = reverse_name.split('')
 
-reverse_string = reverse_name.join(' ')
-# 'Last first' <-- string
+# Pass in reverse letters of name in the alias_method and assign return as the alias name.
+alias_name = alias_method(reverse_letters)
 
-reverse_array = reverse_string.split('')
-# ['L','a','s','t','','F','i','r','s','t']
-
-alias_name = alias_method(reverse_array)
-
-puts alias_name
-
+# Create a key value pair of the real name and alias name to be added to the alias list hash.
 alias_list[real_name] = alias_name
 
-end
-p alias_list
-
+  end
 end
 
+# Iterate through each real name and alias name pair in the alias list hash and print the pair to the user.
 alias_list.each do |real_name, alias_name|
   puts "#{alias_name} is actually #{real_name}"
   end
