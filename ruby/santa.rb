30385b3b6730b5a2688ccb5c2c1d55f6c6dcf64f
miker-mcd <michaelrmcd@gmail.com>
2017-06-12 18:01:55 -0700

    Add instance methods for release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+class Santa
+
+  def speak
+    p "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    p "That was a good #{cookie}!"
+  end
+
+  def initialize
+    p "Initializing Santa instance..."
+  end
+
+end
+
+kris_kringle = Santa.new
+
+kris_kringle.speak
+
+kris_kringle.eat_milk_and_cookies("snickerdoodle")
