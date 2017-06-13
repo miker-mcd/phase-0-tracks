d891c8b20f60f9fdf62d2ce65a5db8410ed416ab
miker-mcd <michaelrmcd@gmail.com>
2017-06-13 11:27:59 -0700

    Add getter and setter methods for release2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,21 +1,66 @@
 class Santa
 
   def speak
-    p "Ho, ho, ho! Haaaappy holidays!"
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie)
-    p "That was a good #{cookie}!"
+    puts "That was a good #{cookie}!"
   end
 
-  def initialize
-    p "Initializing Santa instance..."
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donner","Blitzen"]
+    @age = 0
+    puts "Initializing Santa instance..."
+    puts "New Santa is #{@gender} and #{@ethnicity}"
+  end
+
+  def celebrate_birthday
+    @age + 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking << @reindeer_ranking.slice!(@reindeer_ranking.index(reindeer_name))
+  end
+
+  # Getter Methods
+  def gender
+    @gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  # Setter Methods
+  def gender=(new_gender)
+    @gender = new_gender
   end
 
 end
 
-kris_kringle = Santa.new
+# Driver Code
 
+kris_kringle = Santa.new("non-conforming","Canadian")
+good_grinch = Santa.new("unknown","Whovillain")
+krampus = Santa.new("male","German")
 kris_kringle.speak
-
 kris_kringle.eat_milk_and_cookies("snickerdoodle")
+puts krampus.get_mad_at("Dasher")
+puts kris_kringle.age
+puts good_grinch.gender
+puts krampus.ethnicity
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
