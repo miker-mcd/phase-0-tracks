d6262221c3475b122baf6d93d999a6f5b877cbc1
miker-mcd <michaelrmcd@gmail.com>
2017-06-14 18:33:01 -0700

    Add methods for release 0-2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,46 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    p "Woof!" * int
+  end
+
+  def roll_over
+    p "Rolls over"
+  end
+
+  def dog_years(int)
+    puts "#{int * 7} dog years"
+  end
+
+  def plays_dead(int)
+    p "Bang!" * int
+    puts "I'm dead"
+  end
+
+  def initialize
+    p "Initializing new puppy instance..."
+  end
+
 end
 
+# Puppy.new.fetch("ball")
+# Puppy.new.speak(3)
+# Puppy.new.roll_over
+# Puppy.new.dog_years(5)
+# Puppy.new.plays_dead(3)
+
+class Clown
+
+  def juggles(int)
+    puts "I'm juggling #{int} balls!"
+  end
+
+  def joke(str)
+    puts "Why did #{str} cross the road!?"
+    puts "Cuz the light was green!"
+  end
+
+  def initialize
+
+end
