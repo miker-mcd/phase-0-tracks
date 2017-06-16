79826b0d7a61dca08b02f93e82df1021280b880a
miker-mcd <michaelrmcd@gmail.com>
2017-06-16 10:46:35 -0700

    Add initialize method to word_game file

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,3 +1,7 @@
 class WordGame
 
+  def initialize(word)
+    @player_one_word = word
+  end
+
 end
--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,5 +1,8 @@
 require_relative 'word_game'
 
 describe WordGame do
+  let(:player_one_word) { WordGame.new("secret_word") }
+
+
 
 end
