99465f80bbf2337f2f0eda94f35d0e0210a850d5
miker-mcd <michaelrmcd@gmail.com>
2017-06-16 11:05:19 -0700

    Add pseudocode to word_game file

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,7 +1,25 @@
+# Create an empty container object
+# Create a counter and set it to zero
+# WHILE counter is less than length of the first container AND letter matches a previous letter in the same container
+  # Push the letter of the counter index to the container
+  # Add one to the counter
+# ENDWHILE
+
+# IF the last letter in the second container is the same as any letter in the first container, it is a match (TRUE)
+# DISPLAY letter
+# ELSE the last letter doesn't match (FALSE)
+
 class WordGame
 
   def initialize(word)
     @player_one_word = word
   end
+
+# Create a counter and set it to zero
+# Create an empty container object
+# WHILE counter is less than length of the word
+  # Push letter at the counter index to the container
+  # Add one to the counter
+# ENDWHILE
 
 end
