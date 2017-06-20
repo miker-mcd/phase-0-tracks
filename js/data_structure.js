e0aea8749ba06d5a62f9c823db507376daa07c66
miker-mcd <michaelrmcd@gmail.com>
2017-06-20 12:27:35 -0700

    Add Car constructor for rel. 2

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,3 +1,5 @@
+// RELEASE 1 : BUILD AN OBJECT
+
 var colors = ["blue", "pink", "yellow", "fire red"];
 
 var names = ["Mike", "Lucky", "Winner", "Cruz"];
@@ -15,3 +17,29 @@ for (var i = 0; i < colors.length; i++) {
 };
 
 console.log(horses);
+
+// RELEASE 2 : BUILD MANY OBJECTS USING A CONSTRUCTOR
+
+function Car(make, model, color, num_doors) {
+
+  this.make = make;
+  this.model = model;
+  this.color = color;
+  this.num_doors = num_doors;
+
+  this.honks = function() { console.log("beep!"); };
+
+  console.log("CAR INITIALIZATION COMPLETE");
+};
+
+var newCar = new Car("BMW", "M5", "red", 4);
+console.log(newCar);
+console.log("Our car can honk:");
+newCar.honks();
+console.log("----------")
+
+var anotherCar = new Car("KIA", "Sportage", "gray", 5);
+console.log(anotherCar);
+console.log("Our second car can honk:");
+anotherCar.honks();
+console.log("----------")
