4a09f758a883c04934043e8761754da49ae663d1
miker-mcd <michaelrmcd@gmail.com>
2017-06-20 10:23:03 -0700

    Add test code for reverse string loop

--- js/explore.js
+++ js/explore.js
@@ -3,8 +3,8 @@
 // Reverse string function
 // Input: String
 // Set counter to the length of the string
-// WHILE counter is greater than zero
-  // Print EACH letter of the string starting at the letter that is the same as the length of the string
+// WHILE counter is greater than or equal to zero
+  // Print EACH letter of the string that is the same as the counter
   // Decrease the counter by one
 // ENDWHILE
 // Output: Reversed string
@@ -12,3 +12,7 @@
 // DRIVER CODE
 
 var str = "hello"
+// console.log(str[5]);
+for (var i = (str.length - 1); i >= 0; i--) {
+  console.log(str[i]);
+}
