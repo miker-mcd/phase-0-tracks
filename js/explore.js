f3ebbc4d00f65503f1d99c0cdc85a1d9d79603eb
miker-mcd <michaelrmcd@gmail.com>
2017-06-20 10:08:23 -0700

    Pseudocode reverse string method

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+// PSEUDOCODE
+
+// Reverse string function
+// Input: String
+// Set counter to the length of the string
+// WHILE counter is greater than zero
+  // Print EACH letter of the string starting at the letter that is the same as the length of the string
+  // Decrease the counter by one
+// ENDWHILE
+// Output: Reversed string
+
+// DRIVER CODE
+
+var str = "hello"
