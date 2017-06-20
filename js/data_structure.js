6a8048f45caf4c6c64e7ae957889ed12949d8900
miker-mcd <michaelrmcd@gmail.com>
2017-06-20 11:19:49 -0700

    Add color and names array for rel. 0

---
+++ js/data_structure.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "pink", "yellow", "fire red"];
+
+var names = ["Mike", "Lucky", "Winner", "Cruz"];
+
+colors.push("coral");
+names.push("Champion");
+
+console.log(colors)
+console.log(names)
--- js/explore.js
+++ js/explore.js
@@ -2,17 +2,21 @@
 
 // Reverse string function
 // Input: String
-// Set counter to the length of the string
+// Set counter to the length of the string minus one
 // WHILE counter is greater than or equal to zero
-  // Print EACH letter of the string that is the same as the counter
+  // Add the letter to the letter minus the count
   // Decrease the counter by one
 // ENDWHILE
 // Output: Reversed string
 
 // DRIVER CODE
 
-var str = "hello"
+var str = "hello";
 // console.log(str[5]);
 for (var i = (str.length - 1); i >= 0; i--) {
-  console.log(str[i]);
+  // console.log(str[i]);
+  var new_string = new_string + str[i]
+  // console.log(str[i] += str[i - 1])
 }
+
+console.log(new_string);
