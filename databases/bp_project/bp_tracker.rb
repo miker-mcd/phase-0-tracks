7a8d808c8f0928cf8697e542e50c51aecd02514e
miker-mcd <michaelrmcd@gmail.com>
2017-06-30 13:10:53 -0700

    Create entry and print_table methods for bp_tracker UI

--- databases/bp_project/bp_tracker.rb
+++ databases/bp_project/bp_tracker.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new("bloodpressure.db")
+$DB = SQLite3::Database.new("bloodpressure.db")
+$DB.results_as_hash = true
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS bloodpressure(
@@ -12,4 +13,21 @@ create_table_cmd = <<-SQL
   )
 SQL
 
-db.execute(create_table_cmd)
+$DB.execute(create_table_cmd)
+
+def entry(db, diastolic, systolic, date, time)
+  db.execute("INSERT INTO bloodpressure (diastolic, systolic, date, time) VALUES (?, ?, ?, ?)", [diastolic, systolic, date, time])
+end
+
+def print_table
+  table = $DB.execute("SELECT diastolic, systolic, date FROM bloodpressure")
+  table.each do |entry|
+    puts "#{entry['date']}- Diastolic: #{entry['diastolic']} over #{entry['systolic']}."
+  end
+  # puts "Your blood pressure was #{table[0][1]} over #{table[0][2]}, on #{table[0][3]}."
+end
+
+# TEST CODE
+
+# entry(db, 130, 80, '2017-06-30', '8:00:00')
+print_table
