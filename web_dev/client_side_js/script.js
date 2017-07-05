072ecf6491cd6f559d5e147ef06e0e3a05b9b806
miker-mcd <michaelrmcd@gmail.com>
2017-07-05 16:50:21 -0700

    Add comments to script.js file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,28 +1,35 @@
+// Test the script.js file is running
 console.log("Is this working?");
 
+// Change the font color of the strong element to green
 var greats = document.getElementsByTagName("strong");
 var great = greats[0];
 great.style.color = "green";
 
+// Change the ul background to pink
 var ul_backs = document.getElementsByTagName("ul");
 var ul_back = ul_backs[0];
 ul_back.style.background = "pink";
 
+// Create a new li and add to page
 var newli = document.createElement("li");
 var node = document.createTextNode("relatively quiet");
 newli.appendChild(node);
 ul_back.appendChild(newli);
 
+// Add event listener that hides the lizard photo when user clicks the blend button
 function blend(event) {
   var photo = document.getElementById("lizard-photo");
   photo.style.visibility = "hidden";
 }
 
+// Add event listener that reveals the lizard photo when the user clicks the reveal button
 function reveal(event) {
   var photo = document.getElementById("lizard-photo");
   photo.style.visibility = "visible";
 }
 
+// Test the event listeners
 var button = document.getElementById("button1");
 button.addEventListener("click", blend);
 
