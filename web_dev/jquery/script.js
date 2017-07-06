7cebe118dc9db48397d05e3d37de28368da51c26
miker-mcd <michaelrmcd@gmail.com>
2017-07-06 13:45:23 -0700

    Create styles.css, add ref to index and test

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,9 +1,24 @@
-$( document ).ready(function() {
+// Launching code on document ready
+// $( document ).ready(function() {
 
-    $( "a" ).click(function( event ) {
+//     $( "a" ).click(function( event ) {
 
-        alert( "Thanks for visiting!" );
+//         alert( "Thanks for visiting!" );
 
-    });
+//     });
 
-});
+// });
+
+// $( document ).ready(function() {
+
+//     $( "a" ).click(function( event ) {
+
+//         alert( "As you can see, the link no longer took you to jquery.com" );
+
+//         event.preventDefault();
+
+//     });
+
+// });
+
+// Adding and removing an HTML class
