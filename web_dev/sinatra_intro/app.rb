48cb1ee87ab4138609bd83bfa6898112980d4306
miker-mcd <michaelrmcd@gmail.com>
2017-07-07 10:30:20 -0700

    Create addition route for Rel. 0

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -63,3 +63,10 @@ get '/great_job' do
     "Good job!"
   end
 end
+
+# Write a route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+
+get '/:number_1/plus/:number_2' do
+  result = params[:number_1][0].to_i + params[:number_2][0].to_i
+  "#{params[:number_1]} plus #{params[:number_2]} is #{result}."
+end
